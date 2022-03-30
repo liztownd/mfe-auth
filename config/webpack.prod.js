268e9ebe2a42255cbbdf220f1777b8a2d6a136a9
Liz Townsend <elizabeth.townsend@torch.ai>
2022-03-30 18:12:44 -0500

    nope, definitely out

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,13 +3,11 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const authDomain = process.env.PRODUCTION_AUTH_DOMAIN;
-
 const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: `${authDomain}/auth/latest/`
+        publicPath: '/auth/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
